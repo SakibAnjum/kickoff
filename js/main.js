683c1284469b60b64763edb03368463488e8722b
SakibAnjum <sakib.ask@gmail.com>
2016-11-13 15:12:24 +0600

    Sticky Header Added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 (function ($) {
  "use strict";
 
+
+
+
+
+
+
+ // Menu sticky
+ $(".header").sticky({topSpacing:0});
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*----------------------------
  jQuery MeanMenu
 ------------------------------ */
@@ -21,11 +42,26 @@ var wow = new WOW({
 /*----------------------------
  owl active
 ------------------------------ */
-$('.owl-carousel').owlCarousel({
+$('.kickOff-slider').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
     items:1,
+    autoplayTimeout:10000,
+    autoplay:true,
+    animateIn:'fadeIn',
+    animateOut:'fadeOut',
+    navText:['<i class="icofont icofont-thin-left"></i>','<i class="icofont icofont-thin-right"></i>']
+
+})
+/*----------------------------
+ owl active
+------------------------------ */
+$('.client-logo-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    items:6,
     autoplayTimeout:10000,
     autoplay:true,
     animateIn:'fadeIn',
